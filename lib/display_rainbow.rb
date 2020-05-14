3074a6df45916870fcbf2f923af28f143f8cd00d
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 02:13:46 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   for color in colors do
-    if color != color[-1] 
+    if color != color[-1]
       print("{color[0].upcase}: {color}, ")
     else
       print("{color[0].upcase}: {color}")
