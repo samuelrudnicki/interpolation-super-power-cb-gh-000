a20067f8710c6f8a8fa26dc8857ca5d3d9739214
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-14 02:30:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  result = String.new
+  result = Array.new
   for color in colors do
-    if color != colors[-1]
-      result += "#{color[0].upcase}: #{color}, "
-    else
-      result += "#{color[0].upcase}: #{color}"
-    end
+      result << "#{color[0].upcase}: #{color}"    
   end
-  puts(result)
+  puts(result.join(', '))
 end
